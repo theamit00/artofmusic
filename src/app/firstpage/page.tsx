fa5ca8e826ab621c5d8fba86a1ca888219db8d70
theamit00 <amitmr520@gmail.com>
2024-10-06 12:28:58 +0530

    Navbar added

--- src/app/firstpage/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function firstpage(){
-    return(
-        <h1 className="text-3xl text-center mt-8 " >
-            My First Page
-        </h1>
-    )
-}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
 
 export const metadata: Metadata = {
   title: "Art Of Music",
@@ -12,8 +13,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark" >
       <body>
+        <div className="relative w-full flex items-center justify-center" >
+          <Navbar/>
+        </div>
         {children}
       </body>
     </html>
